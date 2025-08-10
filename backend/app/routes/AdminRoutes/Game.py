7ce31f71621d5a8b7fb0b921c722f25e45493bea
crocodilelurker <swagatsahu556@gmail.com>
2025-08-10 13:56:25 +0530

    Added a endpoint where my admin can create games

--- backend/app/routes/AdminRoutes/Game.py
+++ backend/app/routes/AdminRoutes/Game.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter
-
+from app.models.GameModels.Uplink import GameUplink
+from app.configurations import GameCollection
 router= APIRouter(prefix="/gameAdmin",tags=["Admin Game"])
 
 @router.get("/")
 async def root():
     return {
         "message":"Welcome to Admin GameHome"
+    }
+
+from bson import ObjectId
+
+@router.post("/create-game")
+async def newGame(Game: GameUplink):
+    game_dict = Game.model_dump()
+    result = await GameCollection.insert_one(game_dict)
+
+    # Add the stringified _id into the response object if needed
+    game_dict["_id"] = str(result.inserted_id)
+
+    return {
+        "message": "Game created successfully",
+        "game": game_dict
     }
